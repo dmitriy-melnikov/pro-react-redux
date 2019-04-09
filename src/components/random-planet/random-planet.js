c58f81edfd199b57bb7a94123f1532938b5d90e1
dmitriy-melnikov <dimaira2012@yandex.ru>
2019-04-09 05:35:30 +0300

    spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,64 +1,103 @@
-import React, { Component } from 'react';
-import { random } from 'lodash';
+import React, {Component, Fragment} from "react";
+import { random } from "lodash";
 
-import './random-planet.css';
+import "./random-planet.css";
 
-import SwapiService from '../../services/swapi-service'
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
+
+import SwapiService from "../../services/swapi-service"
+
+const PlanetView = ({planet}) => {
+    console.log("props_Planet", planet);
+    const {
+        id,
+        name,
+        population,
+        rotationPeriod,
+        diameter
+    } = planet;
+    return (
+        <Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg `}/>
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </Fragment>
+    )
+
+}
 
 export default class RandomPlanet extends Component {
-  constructor(props) {
-    super(props);
-    //this.updatePlanet();
-  }
+    swapiService = new SwapiService();
 
-  swapiService = new SwapiService();
-
-  state = {
-    planet: {}
-  };
+    state = {
+        planet: {},
+        loading: true,
+        error: false
+    };
 
 
-  onPlanetLoaded = (planet) => {
-		this.setState({planet})
-  };
+    onPlanetLoaded = (planet) => {
+        this.setState({
+            planet,
+            loading: false
+        })
+    };
 
-  updatePlanet() {
-    const id = random(1, 25);
-    this.swapiService.getPlanet(id)
-      .then(this.onPlanetLoaded)
-  }
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    };
 
-	componentDidMount() {
-		this.updatePlanet()
-	}
+    updatePlanet() {
+        const id = random(1, 25);
+        this.swapiService.getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError)
+    }
+
+    componentDidMount() {
+        this.updatePlanet()
+    }
 
 
-  render() {
-    const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
+    render() {
+        const {
+            planet,
+            loading,
+            error
+        } = this.state;
 
-    return (
-      <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg `}/>
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
-      </div>
+        const errorMessage = error ? <ErrorIndicator/> : null;
+        const spinner = loading ? <Spinner className="ng-scope"/> :  null;
+        const content = !(loading || error) ? <PlanetView planet={planet}/> : null;
 
-    );
-  }
+        return (
+            <div className="random-planet jumbotron rounded">
+                {spinner}
+                {errorMessage}
+                {content}
+            </div>
+        )
+    }
 }
+
+
